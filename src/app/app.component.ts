0455cbc2d932e867930c964f1c98e886f019a9cf
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2023-09-28 18:17:40 -0500

    Added Bootstrap
    added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'exercise_class_one';
   name: string = "Amanda";
+
+  ngOnInit() {
+    
+  }
 }
